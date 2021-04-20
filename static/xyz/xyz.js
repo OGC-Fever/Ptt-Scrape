fe725b5998d9179189ed949c2f8ebf58a3e0afa9
OGC-fever <seal.heart@gmail.com>
2021-04-21 00:54:40 +0800

    test

--- static/xyz/xyz.js
+++ static/xyz/xyz.js
@@ -1,48 +1,29 @@
-const week = Vue.createApp({
+const cal = Vue.createApp({
     delimiters: ['@', '@'],
     data() {
         return {
             week: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-        }
-    },
-}).mount("#week")
-
-const title = Vue.createApp({
-    delimiters: ['@', '@'],
-    data() {
-        return {
-            title: ""
-        }
-    },
-    methods: {
-        init() {
-            let title = new Date().toLocaleString("tw", { year: "numeric", month: "long" }).split(" ")
-            return `${title[0]} / ${title[1]}`
-        }
-    },
-    mounted() {
-        this.title = this.init()
-    }
-}).mount("#title")
-
-const day = Vue.createApp({})
-day.component("day", {
-    delimiters: ['@', '@'],
-    data() {
-        return {
+            title: "",
             month: [],
             weeks: 0
         }
     },
     methods: {
+        month_year() {
+            let title = new Date().toLocaleString("tw", { year: "numeric", month: "long" }).split(" ")
+            this.title = `${title[0]} / ${title[1]}`
+        },
         alert(n, index) {
             let input = prompt("schedule:", "")
             if (input) {
                 this.month[(n - 1) * 7 + index].push(input)
             }
         },
-        init() {
+        get_month() {
             let month = new Date().getMonth()
+            // if (plus) {
+            //     month += plus
+            // }
             let year = new Date().getFullYear()
             let first_day = new Date(year, month, 1).getDay()
             let last_date = new Date(year, month + 1, 0).getDate()
@@ -58,18 +39,25 @@ day.component("day", {
                 this.month.push([""])
             }
             this.weeks = this.month.length / 7
-        }
+        },
     },
     mounted() {
-        this.init()
+        this.month_year()
+        this.get_month()
+    }
+}).component("day", {
+    props: ["weeks", "month"],
+    methods: {
+        alert(n, index) {
+            this.$parent.alert(n, index)
+        }
     },
     template: `
     <div class="row mx-auto" v-for="n in weeks">
         <div class="col inner date" v-for="(,index) in month.slice(7*n-7,7*n)" @click="alert(n,index)">
             <div v-for="(,i) in month.slice(7*n-7,7*n)[index]">
-                @month.slice(7*n-7,7*n)[index][i]@
+                {{month.slice(7*n-7,7*n)[index][i]}}
             </div>
         </div>
     </div>`,
-})
-day.mount("#day")
+}).mount("#calendar")
